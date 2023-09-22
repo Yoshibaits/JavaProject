431627932fdca1346b96f162d9b83d217cfecac3
Yoshibaits <neil_dsg@yahoo.com>
2023-09-23 02:43:50 +0800

    First Save for Java Project

---
+++ FirstJavaProject/src/main/java/com/mycompany/firstjavaproject/FirstJavaProject.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firstjavaproject;
+
+/**
+ *
+ * @author MainPC_gneil
+ */
+public class FirstJavaProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+                
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+        
+        while(running){
+            System.out.println("Hello World!");
+            System.out.println("***********************");
+            System.out.println("* Welcome to My Shop! *");
+            System.out.println("***********************");
+            System.out.println("1 - Login");
+            System.out.println(".......................");
+            System.out.println("0 - Exit");
+            System.out.print("What do you want to do?");
+            
+            String input = scanner.nextLine();
+            
+            switch (input) {
+                case "1":
+                    System.out.println("Login part");
+                    break;
+                case "2":
+                    System.out.println("Thank you for using our services!");
+                    
+                default:
+                    System.out.println("Invalid choice.");
+            }
+    }
+}
